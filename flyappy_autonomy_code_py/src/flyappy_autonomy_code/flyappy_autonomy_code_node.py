784bb78e1f48cdb4b7b77ce811df24a04bcee6a3
Lorenzo Ortolani <132085189+Relo02@users.noreply.github.com>
2026-01-31 15:52:25 +0100

    Remove MPCNode instantiation from main function
    Commented out the MPCNode instantiation in main function.

--- flyappy_autonomy_code_py/src/flyappy_autonomy_code/flyappy_autonomy_code_node.py
+++ flyappy_autonomy_code_py/src/flyappy_autonomy_code/flyappy_autonomy_code_node.py
@@ -2,17 +2,13 @@ import rclpy
 
 # Available controllers:
 from flyappy_autonomy_code.flyappy_ros import FlyappyRos
-from flyappy_autonomy_code.new_mpc import MPCNode
 
 def main() -> None:
     rclpy.init()
     node = rclpy.node.Node('flyappy_autonomy_code_py')
 
     FlyappyRos(node)  # Alternative: MPC-based controller
-    # MPCNode(node)
-
     rclpy.spin(node)
-
 
 if __name__ == '__main__':
     main()
